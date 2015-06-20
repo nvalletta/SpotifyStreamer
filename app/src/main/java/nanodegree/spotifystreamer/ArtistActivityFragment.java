6c0c90a9275bef1339c9d59f77a9c64c1c2514a7
nvalletta <nora.valletta@gmail.com>
2015-06-20 10:56:19 -0600

    Initial commit

---
+++ app/src/main/java/nanodegree/spotifystreamer/ArtistActivityFragment.java
@@ -0,0 +1,23 @@
+package nanodegree.spotifystreamer;
+
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ArtistActivityFragment extends ListFragment {
+
+    public ArtistActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_artist, container, false);
+    }
+}
