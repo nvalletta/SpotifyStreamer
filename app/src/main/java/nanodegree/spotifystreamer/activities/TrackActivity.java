8db9536ec36714a518633160c867bd5076e22247
nvalletta <nora.valletta@gmail.com>
2015-06-28 10:41:11 -0600

    TrackActivity and activity_artists refactor

---
+++ app/src/main/java/nanodegree/spotifystreamer/activities/TrackActivity.java
@@ -0,0 +1,19 @@
+package nanodegree.spotifystreamer.activities;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import nanodegree.spotifystreamer.R;
+
+public final class TrackActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_artists);
+    }
+
+
+}
